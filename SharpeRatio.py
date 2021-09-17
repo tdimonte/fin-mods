bd2c5db69c89d917e0805e22ceaa3853db1ac764
tdimonte <tdimonte@luc.edu>
2021-09-16 20:57:00 -0500

    Created SharpeRatio.py, Added Sharpe ratio backround

---
+++ SharpeRatio.py
@@ -0,0 +1,15 @@
+#The Sharpe ratio was developed by Nobel laureate William F. Sharpe and is used to help investors understand the return of an investment compared to its risk. The ratio is the average return earned in excess of the risk-free rate per unit of volatility or total risk. Volatility is a measure of the price fluctuations of an asset or portfolio.
+
+#Rp = return of portfolio
+#Rf = risk-free rate
+#σp = standard deviation of the portfolio's excess return
+
+
+
+
+
+
+
+
+
+
