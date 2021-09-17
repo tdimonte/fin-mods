026c915a6c05bd7c15838bf99396caa913ccc56e
tdimonte <tdimonte@luc.edu>
2021-09-17 16:52:32 -0500

    Created hpr.py, updated to snakecase

---
+++ hpr.py
@@ -0,0 +1,5 @@
+#Holding period return is the total return received from holding an asset or portfolio of assets over a period of time, known as the holding period, generally expressed as a percentage (Investopedia).
+
+
+
+
